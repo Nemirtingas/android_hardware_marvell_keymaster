5a3a5c8c78e374fd865f363663193eb8965c2a78
Nemirtingas <nanaki89@hotmail.fr>
2018-12-31 17:43:15 +0100

    PXA1908 keymaster sources

---
+++ pxa1908/Android.mk
@@ -0,0 +1,35 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
+ifeq ($(TARGET_BOARD_SOC),pxa1908)
+
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := keystore.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_SRC_FILES := keymaster_mrvl.cpp
+
+LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/common/inc \
+                    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_CFLAGS := -fvisibility=hidden -Wall
+
+LOCAL_SHARED_LIBRARIES := \
+        libtee_client \
+        libkeystore_binder \
+        libcrypto \
+        liblog \
+        libstdc++ \
+        libc
+
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr \
+    $(LOCAL_PATH)/Android.mk
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif # TARGET_BOARD_SOC
+endif # TARGET_PROVIDES_KEYMASTER
